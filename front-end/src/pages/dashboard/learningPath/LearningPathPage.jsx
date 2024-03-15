362556e3c7ced4533e4868e3df0e19b33dc8a8b4
Amir Cherif <97757872+amircherif-ac@users.noreply.github.com>
2024-03-15 12:28:47 -0400

    CRUD + UI
    UI data fetch from the backend is fully functional with a very light version of metrics.

--- front-end/src/pages/dashboard/learningPath/LearningPathPage.jsx
+++ front-end/src/pages/dashboard/learningPath/LearningPathPage.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Typography } from '@mui/material';
 
 const LearningPathPage = (props) => {
+    const [userTimeSpent, setUserTimeSpent] = useState([]);
 
-    // async function getUserTimeSpent() {
-    //     const response = await fetch('http://localhost:5000/api/timespent/user/{props.thisUser.id}');
-    //     const resData = await response.json();
-    //     return resData;
-    // }
+    useEffect(() => {
+        async function fetchUserTimeSpent() {
+            try {
+                const response = await fetch(`http://localhost:5000/api/timespent/user/${props.thisUser.id}`);
+                const resData = await response.json();
+                setUserTimeSpent(resData);
+            } catch (error) {
+                console.error('Error fetching user time spent:', error);
+            }
+        }
+
+        fetchUserTimeSpent();
+    }, []);
 
     return (
         <div className="h-full w-full p-5 flex flex-row flow-up-animation">
@@ -20,7 +29,7 @@ const LearningPathPage = (props) => {
                         <Typography variant="h5">Enrolled Courses:</Typography>
                         {props.enrolledCourses.map(course => (
                             <li key={course.courseId}>
-                            <Typography variant="h7">{course.courseId} {course.courseTitle}</Typography>
+                                <Typography variant="h7">{course.courseId} {course.courseTitle}</Typography>
                             </li>
                         ))}
                     </Container>
@@ -29,11 +38,11 @@ const LearningPathPage = (props) => {
                         <Typography variant="h5">=============================</Typography>
                         <Typography variant="h5">User Dashboard API Testing...</Typography>
                         <Typography variant="h5">=============================</Typography>
-                        {/* {getUserTimeSpent().map(timespent => (
-                            <li key={timespent.logID}>
-                            <Typography variant="h7">{timespent.logID} {timespent.date}</Typography>
+                        {userTimeSpent.map(log => (
+                            <li key={log.logID}>
+                                <Typography variant="h7">{log.logID} - {log.timeSpentLog} - {log.date} </Typography>
                             </li>
-                        ))} */}
+                        ))}
                     </Container>
                 </div>
             </div>
