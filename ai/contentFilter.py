ecf68e315c25b941f6c6fef9f4a918987f98d995
Amish <amish920w@gmail.com>
2024-03-20 05:55:33 -0400

    all recommendations

--- ai/contentFilter.py
+++ ai/contentFilter.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
-from fetch_data import get_all_courses
+from fetch_data import get_all_courses, get_enrolled_courses
 
 # ============================== Flask ==============================
 from flask import Flask
@@ -16,18 +16,25 @@ port = 5007
 # ============================== Flask ==============================
 
 logedInUserID = 1
+enrolled_courses = get_enrolled_courses(logedInUserID)
+print("User enrolled courseID: ", enrolled_courses)
 
 # Content based filtering
 
 # read courses and user tags
-#course = pd.read_csv("Course_Desc.csv", header=0)
-courses_data = get_all_courses()
+# courses_data = pd.read_csv("Course_Desc.csv", header=0)
+# courses_data = get_all_courses()
 
-# Filter courses to extract 
+# Filter all courses to extract 
 courses_extracted = [{'courseId': course['courseId'], 'courseTitle': course['courseTitle'], 'description': course['description']} for course in courses_data]
 
 # Convert filtered data into a DataFrame
 courses_df = pd.DataFrame(courses_extracted)
+# print("courses_df ", courses_df)
+
+# Filter course based on enrolled course of userId
+enrolled_courses_df = courses_df[courses_df['courseId'].isin(enrolled_courses)]
+print("enrolled_courses_df: ", enrolled_courses_df)
 
 # Display DataFrames
 print("Courses DataFrame:")
@@ -50,10 +57,6 @@ similarity_matrix = cosine_similarity(tfidf_matrix)
 
 
 # Clustering
-# Regression kNN will be used for classification of empty tags (columns)
-# elbow method can help determine a suitable number by plotting the within-cluster sum of squares (WCSS)
-# against the number of clusters abd looking for the elbow point where the rate of decrease sharply changes
-
 # Assign num of clusters
 kmeans = KMeans(n_clusters=10)
 kmeans.fit(tfidf_matrix)
@@ -63,8 +66,8 @@ print("k labels", kmeans.labels_)
 courses_df['cluster'] = kmeans.labels_
 
 # Reduce dimensions
-pca = PCA(n_components=2)
-reduced_data = pca.fit_transform(tfidf_matrix.toarray())
+# pca = PCA(n_components=2)
+# reduced_data = pca.fit_transform(tfidf_matrix.toarray())
 
 # # Plot each cluster
 # fig = go.Figure()
@@ -122,47 +125,57 @@ def print_term_idf(title):
 
 
 # Function to recommend courses based on clusters
-def recommend_courses_by_cluster(courseTitle, top_n=5):
+def recommend_courses_by_cluster(courseId, top_n=5):
     # Find the cluster label of the given course
-    course_cluster = courses_df.loc[courses_df['courseTitle'] == courseTitle, 'cluster'].values[0]
+    course_cluster = courses_df.loc[courses_df['courseId'] == courseId, 'cluster'].values[0]
 
     # Filter courses from the same cluster, excluding the queried course
     similar_courses = courses_df[courses_df['cluster'] == course_cluster]
-    similar_courses = similar_courses[similar_courses['courseTitle'] != courseTitle]
+    similar_courses = similar_courses[similar_courses['courseId'] != courseId]
 
     # Return top N similar courses
     return similar_courses[['courseId', 'courseTitle']].head(top_n)
 
+def get_all_recommendations():
+    all_recommendations = pd.DataFrame(columns=['courseId', 'courseTitle'])  
+    for courseId in enrolled_courses:
+        recommendations = recommend_courses_by_cluster(courseId)
+        all_recommendations = pd.concat([all_recommendations, recommendations], ignore_index=True)  # Concatenate the recommendations
+        all_recommendations = all_recommendations.drop_duplicates().reset_index(drop=True)
+
+    return all_recommendations
 
 # Test the function
-course_title = "Digital Systems Design I"
+course_title = "Microprocessor Systems"
 print_term_idf(course_title)
 
 # Test the function
 print("\n Recommend courses by description:")
 print(recommend_courses_by_description(course_title))
 
+# coen 313 = 391 = "Microprocessor Systems"
+print("\n Recommend courses based on cluster:")
+print(recommend_courses_by_cluster(391))
+
 # Test the function
 print("Recommend courses based on cluster:")
-print(recommend_courses_by_cluster(course_title))
+# Recommend similar courses for each enrolled course
+# Combine them and remove duplicates
+all_recommendations = get_all_recommendations()
+print(all_recommendations)
+
 
 # ============================== Flask ==============================
 @app.get('/suggestion')
 def suggestion():
-    arrResult = recommend_courses_by_cluster(course_title)
-    arrResult_json = arrResult.to_json(orient='records')
-    return arrResult_json
+    all_recommendations = get_all_recommendations()
+    all_recommendations_json = all_recommendations.to_json(orient='records') 
+    return all_recommendations_json
 
 if __name__ == '__main__':
     app.run(debug=False, port=port)
 # ============================== Flask ============================== 
 
-
-# Regression kNN
-# calculate precision and recall
-# A. For content based filtering
-# B. For classification
-# C. For filtering within classification
 
 # Accuracy = ( TruePositive + TrueNegative ) / ( P + N )
 # Recall = TruePositive / ( TruePositive + FalseNegative )
