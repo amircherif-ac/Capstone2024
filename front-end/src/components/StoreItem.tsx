8e2762fb2d282ca819a4d7bf215e78cd216d0ded
kilobeaw <asj.lij@gmail.com>
2024-03-20 15:45:22 -0400

    Store front page Formatting
    -Reused Study Hero styling for buttons and font
    -Added modal for purchasing badge

--- front-end/src/components/StoreItem.tsx
+++ front-end/src/components/StoreItem.tsx
@@ -73,9 +73,10 @@
 import React from "react";
 import { formatCurrency } from "../utilities/formatCurrency";
 import { useShoppingCart } from "../context/ShoppingCartContext";
-import { Card, Button, Typography, Grid } from "@mui/material";
+import { Card, Button, Typography, Grid, Dialog, Box } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Modal from "@mui/material/Modal";
 
 type StoreItemProps = {
   id: number;
@@ -95,8 +96,22 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   const styles = {
     media: {
       height: "auto",
+      padding: "40px",
     },
   };
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   // const quantity = getItemQuantity(id);
 
   return (
@@ -110,11 +125,42 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         />
         <CardContent className="d-flex flex-column">
           <Typography className="d-flex justify-content-between align-items-baseline mb-4">
-            <span className="fs-2">{name}</span>
-            <p>
-              <span className="ms-2 text-muted">{formatCurrency(price)}</span>
-              <Button className="w-100 float-right">Purchase</Button>
-            </p>
+            <span className="fs-2 font-jakarta-sans text-xl">{name}</span>
+            <div>
+              <p className="font-jakarta-sans text-xl">
+                <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                <Button
+                  onClick={handleOpen}
+                  variant="contained"
+                  className="font-jakarta-sans bg-primary hover:bg-blue-500 duration-300 float-right"
+                >
+                  Purchase
+                </Button>
+              </p>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography
+                    className="font-jakarta-sans text-xl"
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    Do you want to confirm purchase?
+                  </Typography>
+                  <Button className="w-100 font-jakarta-sans hover:bg-blue-500 duration-300">
+                    Yes
+                  </Button>
+                  <Button className="w-100 font-jakarta-sans hover:bg-blue-500 duration-300">
+                    No
+                  </Button>
+                </Box>
+              </Modal>
+            </div>
           </Typography>
           <div className="mt-auto">
             {/* {quantity === 0 ? (
--- front-end/src/pages/dashboard/rewards/Store.tsx
+++ front-end/src/pages/dashboard/rewards/Store.tsx
@@ -54,14 +54,6 @@ const Store: React.FC<StoreProps> = () => {
           </Grid>
         ))}
       </Grid>
-      {/* <h1>Rewards</h1>
-      <Row md={2} xs={1} lg={3} className="g-3">
-        {storeItems.map((item) => (
-          <Col key={item.id}>
-            <StoreItem {...item} />
-          </Col>
-        ))}
-      </Row> */}
     </>
   );
 };
