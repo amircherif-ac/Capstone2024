dbfca275861a0df661df5d17be2659646eda1ed2
Amir Cherif <97757872+amircherif-ac@users.noreply.github.com>
2024-03-20 16:59:12 -0400

    Update RecommendationPage.jsx

--- front-end/src/pages/dashboard/Recommendation/RecommendationPage.jsx
+++ front-end/src/pages/dashboard/Recommendation/RecommendationPage.jsx
@@ -42,12 +42,11 @@ const LearningPathPage = (props) => {
         }
     }
 
-
     return (
         <div className="h-full w-full p-5 flex flex-row flow-up-animation">
             <div className="h-full w-full">
                 <div className="bg-white h-full rounded-xl shadow-slate-500 shadow-md flex flex-col mr-5 overflow-hidden">
-                    <Container>
+                    <Container style={{ overflowY: 'scroll', maxHeight: '100%' }}>
                         {recommendation.map((course, index) => (
                             <Card key={index} className="my-4 p-4">
                                 <div className="bg-primary p-5 text-white px-4 py-2 rounded-t-md">
@@ -56,10 +55,12 @@ const LearningPathPage = (props) => {
                                 <Typography variant="body1"> <br/>
                                 {course.description}
                                 </Typography>
-                                <button className="bg-primary p-5 text-white px-4 py-2 mt-2 rounded-md" onClick={() => enroll(course.courseId)}>Enroll</button>
+                                <button className="bg-primary p-5 text-white hover:bg-blue-500 duration-300 px-4 py-2 mt-2 rounded-md" 
+                                onClick={() => enroll(course.courseId)}
+                                variant="contained"
+                                >Enroll</button>
                             </Card>
                         ))}
-
                     </Container>
                 </div>
             </div>
